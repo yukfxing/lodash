bd099e37cee260c70c8fbd5b04d3c1c889cd1e3c
yukfxing <31976198+yukfxing@users.noreply.github.com>
2021-05-17 20:15:06 +0800

    Update isKey.js

--- .internal/isKey.js
+++ .internal/isKey.js
@@ -12,14 +12,19 @@ const reIsPlainProp = /^\w*$/
  * @param {Object} [object] The object to query keys on.
  * @returns {boolean} Returns `true` if `value` is a property name, else `false`.
  */
+// 判断value能否做object的key
 function isKey(value, object) {
+//   数组不可以做key
   if (Array.isArray(value)) {
     return false
   }
+  
   const type = typeof value
+//   基础类型可以做key
   if (type === 'number' || type === 'boolean' || value == null || isSymbol(value)) {
     return true
   }
+//   只包含字母的字符串可以做key，包含[],()的字符串不可以做key，判断是否是object真的key
   return reIsPlainProp.test(value) || !reIsDeepProp.test(value) ||
     (object != null && value in Object(object))
 }
