ba9598168a1755f466bea62ef1948a347acbcb83
yukfxing <31976198+yukfxing@users.noreply.github.com>
2021-05-17 20:22:17 +0800

    Update isFlattenable.js

--- .internal/isFlattenable.js
+++ .internal/isFlattenable.js
@@ -10,7 +10,11 @@ const spreadableSymbol = Symbol.isConcatSpreadable
  * @param {*} value The value to check.
  * @returns {boolean} Returns `true` if `value` is flattenable, else `false`.
  */
+// 判断是否可以扁平化
 function isFlattenable(value) {
+//   数组可以扁平化
+//   argument可以扁平化 类似对象，并且是argument
+//   Symbol
   return Array.isArray(value) || isArguments(value) ||
     !!(value && value[spreadableSymbol])
 }
