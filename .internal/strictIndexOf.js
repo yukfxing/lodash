af430415c1dfc762335a36b8d1ba8230b2309bf3
yukfxing <31976198+yukfxing@users.noreply.github.com>
2021-05-27 19:14:58 +0800

    Update strictIndexOf.js

--- .internal/strictIndexOf.js
+++ .internal/strictIndexOf.js
@@ -8,6 +8,7 @@
  * @param {number} fromIndex The index to search from.
  * @returns {number} Returns the index of the matched value, else `-1`.
  */
+// 从某一个位置开始查找值在数组中的位置，找不到返回-1
 function strictIndexOf(array, value, fromIndex) {
   let index = fromIndex - 1
   const { length } = array
